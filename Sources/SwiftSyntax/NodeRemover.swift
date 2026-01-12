3e84a2040e1ccc8e150c2dddc96b4542ec94aea5
Kirill Gusev <myaumuraa@gmail.com>
2026-01-12 13:05:45 +0300

    fix file comment

--- Sources/SwiftSyntax/NodeRemover.swift
+++ Sources/SwiftSyntax/NodeRemover.swift
@@ -1,9 +1,14 @@
+//===----------------------------------------------------------------------===//
 //
-//  File.swift
-//  swift-syntax
+// This source file is part of the Swift.org open source project
 //
-//  Created by Кирилл Гусев on 11.01.2026.
+// Copyright (c) 2014 - 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 
